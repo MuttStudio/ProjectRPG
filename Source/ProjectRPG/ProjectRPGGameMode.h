669e2d7f64ee7e7b47990ddc6c40bb3a88c86b8f
Russell <Russell.J.Baker@gmail.com>
2014-05-10 11:13:22 -0400

    updated the UDK mappiongs

---
+++ Source/ProjectRPG/ProjectRPGGameMode.h
@@ -0,0 +1,14 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+#pragma once
+
+#include "ProjectRPGGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AProjectRPGGameMode : public AGameMode
+{
+	GENERATED_UCLASS_BODY()
+
+};
+
+
+
